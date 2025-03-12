4a7b80c6729be0141d2a7528b161cbcd986de440
Egor <86737447+EgorPy@users.noreply.github.com>
2025-03-12 23:15:58 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-javac -cp "sqlite-jdbc-3.42.0.0.jar;." Api.java Database.java Utils.java RouteFinder.java
+javac -cp "backend/sqlite-jdbc-3.42.0.0.jar;." backend/Api.java backend/Database.java backend/Utils.java backend/RouteFinder.java
